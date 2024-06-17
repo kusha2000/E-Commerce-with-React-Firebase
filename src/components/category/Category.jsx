4ae97ae678a82f89c10e445f16db5792d87c5c38
kusha2000 <kushanandarawewa1@gmail.com>
2024-06-17 23:43:50 +0530

    Add Updates

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -46,7 +46,7 @@ const Category = () => {
                         {category.map((item, index) => {
                             return (
                                 <div key={index} className="px-3 lg:px-10">
-                                    <div onClick={() => navigate(`/category/${item.name}`)} className=" w-16 h-16 lg:w-24 lg:h-24 max-w-xs rounded-full  bg-pink-500 transition-all hover:bg-pink-400 cursor-pointer mb-1 "  >
+                                    <div onClick={() => navigate(`/category/${item.name}`)} className=" w-16 h-16 lg:w-24 lg:h-24 max-w-xs rounded-full  bg-blue-500 transition-all hover:bg-blue-400 cursor-pointer mb-1 "  >
                                         <div className="flex justify-center mb-12">
                                             <img src={item.image} alt="img" />
                                         </div>
